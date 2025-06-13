de35cf9e4862dcc1a754bfb31ffadf9981c6559c
ACT-C1-17 <ACT-C1-17@DESKTOP-I4SCAC5>
2025-06-13 10:25:10 +0530

    This a basics of character program in java

--- C2TCCoreJavaprograms/src/day1/SanthoshJava.java
+++ C2TCCoreJavaprograms/src/day1/SanthoshJava.java
@@ -1,11 +1,21 @@
 package day1;
-
+import java.util.Scanner;
 public class SanthoshJava {
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
-		System.out.println("Dhanush ");
+		int a,b,c;
+		Scanner ac;
+		ac= new Scanner(System.in);          
+		System.out.println("enter a ");
+		a = ac.nextInt();
+		System.out.println("enter b");
+		b = ac.nextInt();
+		c = a+b;
+		System.out.println("additon of a+b"+" "+c);
+		
+		
+		
 	}
 
 }
