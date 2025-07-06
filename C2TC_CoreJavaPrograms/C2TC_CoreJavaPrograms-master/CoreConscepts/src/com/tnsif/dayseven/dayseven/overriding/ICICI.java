a55aee37c46588476e3d7a25bd268ae3448bf118
Santhosh2432 <santhoshkumar243276@gmail.com>
2025-07-06 18:56:26 +0530

    This My Core Java Programs

---
+++ C2TC_CoreJavaPrograms/C2TC_CoreJavaPrograms-master/CoreConscepts/src/com/tnsif/dayseven/dayseven/overriding/ICICI.java
@@ -0,0 +1,10 @@
+//Program to demonstrate method overriding - Runtime Polymorphism
+package com.tnsif.dayseven.dayseven.overriding;
+//subclass
+public class ICICI extends RBI {
+	@Override
+	public float getRateOfInterest() {
+		return 6.9f;
+	}
+
+}
